93a099c36aca163de50f5a272352fe2bd475af8f
Samuel Koob <56673835+sam-k0@users.noreply.github.com>
2021-12-25 00:02:00 +0100

    add doxygen for gms.h

--- gms.h
+++ gms.h
@@ -31,46 +31,77 @@
             (In this case, it just returns double the value it got.)
 */
 
+/* Header Guards*/
 #ifndef GMS_H_INCLUDED
 #define GMS_H_INCLUDED
-#define gmx extern "C" __declspec(dllexport)
+
+/* Define extern export for GameMaker */
+#define gmx extern "C" __declspec(dllexport) 
+
+/* Define GameMaker Booleans */
 #define gmtrue 1.0;
 #define gmfalse 0.0;
-typedef double gmint;
-typedef const char* stringToDLL; // Pass to dll
-typedef char* stringFromDLL;      // return from dll
-typedef double gmbool;
 
+/* Define GameMaker datatypes*/
+typedef double gmint;               // Integer for GM
+typedef const char* stringToDLL;    // String passed GM --> DLL
+typedef char* stringFromDLL;        // String passed DLL -- > GM
+typedef double gmbool;              // Datatype for GM Boolean
+
+/* Regular includes */
 #include <string>
 #include <string.h>
 #include <iostream>
-using namespace std;
+using namespace std; // Fight me
 
+/**
+* @brief This is the static helper class.
+*        Access is namespace-like: gmu::function
+*/
 class gmu {
 
 public:
 
+    /**
+    * @param s String to convert to char* 
+    * @brief Convert string to char*
+    */
 	static char* string_to_charptr(string s)
 	{
 		return _strdup(s.c_str());
 	}
 
+    /**
+    * @param s String to convert to const char* 
+    * @brief Convert string to const char*
+    */
 	static const char* string_to_constcharptr(string s)
 	{
 		return s.c_str();
 	}
 
+    /**
+    * @param p const char* to convert
+    * @brief Convert const char* to string
+    */
 	static string constcharptr_to_string(const char* p)
 	{
 		return string(p);
 	}
 
+    /**
+    * @param s String to print
+    * @brief Prints a string to GMS debug console
+    */
 	static void debugmessage(string s)
 	{
 		std::cout << s << std::endl;
 	}
 
 private:
+    /**
+    *   @brief Do not instantiate.
+    */
     gmu(){}
 };
 #endif // AMOGUS_H_INCLUDED
